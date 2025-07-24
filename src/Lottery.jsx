5e9703c6bf06265ddde55cf3ffeea6d9fa600056
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-07-24 11:58:30 +0530

    Build: Basic Lottery App using React components, props, useState & events

---
+++ src/Lottery.jsx
@@ -0,0 +1,10 @@
+const Lotter = () => {
+  return (
+    <div>
+      <h1>Lottery Ticket</h1>
+      <p>Welcome to the lottery ticket application!</p>
+    </div>
+  );
+};
+
+export default Lotter;
