6077d85248f83cf7b37cd4e878e5c8d15e8b23a3
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-07-25 12:15:42 +0530

    add some functions

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,10 +1,21 @@
-const Lotter = () => {
+import { useState } from "react";
+import "./Lottery.css";
+import { generateTicket } from "./helper";
+
+const Lottery = () => {
+  let [ticket, setTicket] = useState([generateTicket(3)]);
+  let isWinning = sum(ticket);
+
   return (
     <div>
       <h1>Lottery Ticket</h1>
-      <p>Welcome to the lottery ticket application!</p>
+      <div className="ticket">
+        <span>{tickets[0]}</span>
+        <span>{tickets[1]}</span>
+        <span>{tickets[2]}</span>
+      </div>
     </div>
   );
 };
 
-export default Lotter;
+export default Lottery;
